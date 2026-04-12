f1fe5ef4069bf98633d70b1e55d21615d3b472a9
Ermakov Alexei <lioshaermakov32@gmail.com>
2026-04-12 19:39:40 +0300

    seed for perf tochilin

--- tasks/tochilin_e_hoar_sort_sim_mer/tests/performance/main.cpp
+++ tasks/tochilin_e_hoar_sort_sim_mer/tests/performance/main.cpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <cstddef>
+#include <cstdint>
 #include <random>
 
 #include "tochilin_e_hoar_sort_sim_mer/all/include/ops_all.hpp"
@@ -14,6 +15,12 @@
 
 namespace tochilin_e_hoar_sort_sim_mer {
 
+namespace {
+
+constexpr std::uint32_t kPerfSeed = 0x5A17C3D2U;
+
+}  // namespace
+
 class TochilinEHoarSortSimMerRunPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
  protected:
   const int k_count = 2000000;
@@ -21,7 +28,7 @@ class TochilinEHoarSortSimMerRunPerfTests : public ppc::util::BaseRunPerfTests<I
 
   void SetUp() override {
     input_data.resize(static_cast<std::size_t>(k_count));
-    std::mt19937 gen(std::random_device{}());
+    std::mt19937 gen(kPerfSeed);
     std::uniform_int_distribution<> dis(-10000, 10000);
 
     for (int i = 0; i < k_count; ++i) {
